ca7a9f6b523b2b8f4af57150f64f9393f70fac36
Máximo Lanza <54601369+maximolanza@users.noreply.github.com>
2020-01-02 00:45:28 -0300

    Text CV fix

--- src/app/constant/experiencia.ts
+++ src/app/constant/experiencia.ts
@@ -4,26 +4,26 @@ export const experiencia =[{
     "hasta" : "actualidad",
     "caego" : "Desarrollador GeneXus - Sporte de 2do nivel",
     "Tareas" : [
-                { "descripcion" : "Desarrollo GeneXus (nuevas funcionalidades y modificación de funcionalidades existetes)."},
-                { "descripcion" : "Control y seguimiento de incidentes."},
-                { "descripcion" : "Analisis de datos con SQL Server Management."},
-                { "descripcion" : "Integración de grupo de innovación del sector de Desarrollo."},
-                { "descripcion" : "Atención telefónica de 2do nivel y acceso remoto."},
-                { "descripcion" : "Generación de reportes SQL."},
-                { "descripcion" : "Control de incidentes en Mirth"},
-                { "descripcion" : "Control de incidentes en Modulab"} ]
+                { "descripcion" : "Desarrollo aplicaciones web con GeneXus y participo en la actualización de aplicaciones web existentes en GeneXus Evo1, Evo2 y Evo3."},
+                { "descripcion" : "Realizo control y seguimiento de incidentes en el gestor GLPI apoyandome con SQL Server Management para el análisis de la información y las Knowleadge Bases de distintos sistemas para la revisión de código."},
+                { "descripcion" : "Me encargo de la centralización de las llamadas del sector de Desarrollo como 2do nivel de Soporte a los sistemas."},
+                { "descripcion" : "Soy responsable de generar scripts SQL para los reportes solicitados de urgencia elevada, pre-automatización."},
+                { "descripcion" : "Realizo controles de integración de sistemas haciendo seguimientos de canales en Mirth para mantener una consistencia de los servicios.."},
+                { "descripcion" : "Integro el grupo de innovación del sector de Desarrollo."},
+                { "descripcion" : "Me encargo de controlar el correcto funcionamiento y contínua comunicación de información  de integraciones mediante Mirth, con sistemas de proveedores."} ]
    },
    {
     "empresa" : "Emestudio LTDA",
     "desde" : "2015-01-01",
-    "hasta" : "2016-04-10",
+    "hasta" : "2016-05-01",
     "caego" : "Soporte técnico - Capacitador",
     "Tareas" : [
-                { "descripcion" : "Soporte técnico de Hardware y Software."},
-                { "descripcion" : "Cableado"},
-                { "descripcion" : "Capacitacion de SoftRestorant."},
-                { "descripcion" : "Conducción de vehiculo de la empresa"},
-                { "descripcion" : "Guardia rotativa 24hs"}]
+                { "descripcion" : "Me encargué de realizar soprote técnico a clientes de forma remota y presencial de Software y Hardware."},
+                { "descripcion" : "Realizé la preparación de equipos y la instalación física de todo lo necesario para armar las redes para los clientes."},
+                { "descripcion" : "Era responsable de la capacitacion de SoftRestorant a todo el personal de los clientes."}//,
+               // { "descripcion" : "Conducí los vehiculos de la empresa para asistir a los locales para realizar soporte, instalación y cadetería de suministros informáticos."},
+              //  { "descripcion" : "Era de mi responsabilidad realizar una guardia rotativa 24hs de 1 semana por mes."}
+            ]
    }]
                 
 
